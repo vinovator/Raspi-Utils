e6e2f0f613de4dfcbf592bb82ec2ad56c4020a27
vinovator <vinovator@yahoo.co.in>
2016-04-19 21:45:52 +0100

    added mailmap file

--- mailmap.py
+++ mailmap.py
@@ -6,8 +6,13 @@ Python script to track the mapping between subject and response
 Raspberry pi will use this mapping file to determine relevant response
 """
 
+# Utility library, relative with respect to the calling script
+from find_my_public_ip import get_ip
+
 # List of predefined questions and responses for raspberry pi
 mail_map = {
     "hi": "hi there",
-    "test": "Roger !"
+    "test": "Roger",
+    "where are you": get_ip()  # function call
 }
+
