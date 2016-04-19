617ce9f65065b9b092d36863b5ed16b7ea375be4
vinovator <vinovator@yahoo.co.in>
2016-04-19 21:31:51 +0100

    add mailmap file

---
+++ mailmap.py
@@ -0,0 +1,13 @@
+# mailmap.py
+# Python 3.5
+
+"""
+Python script to track the mapping between subject and response
+Raspberry pi will use this mapping file to determine relevant response
+"""
+
+# List of predefined questions and responses for raspberry pi
+mail_map = {
+    "hi": "hi there",
+    "test": "Roger !"
+}
